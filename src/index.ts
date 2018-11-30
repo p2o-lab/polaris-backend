c2b74348e2641ce6d1908b83ba1173685beca7c8
Markus Graube <markus.graube@tu-dresden.de>
2018-11-30 12:28:58 +0100

    new MTP
    Signed-off-by: Markus Graube <markus.graube@tu-dresden.de>

--- src/index.ts
+++ src/index.ts
@@ -24,11 +24,13 @@
  */
 
 import * as http from 'http';
-import { Server } from './server/server';
+import {Server} from './server/server';
 import * as serverHandlers from './server/serverHandlers';
 import * as commandLineArgs from 'command-line-args';
 import * as fs from 'fs';
-import { manager } from './model/Manager';
+import {manager} from './model/Manager';
+import {watchFlag} from './server/flagWatcher';
+import commandLineUsage = require('command-line-usage');
 
 const optionDefinitions = [
     {
@@ -45,7 +47,7 @@ const optionDefinitions = [
         alias: 'r',
         type: String,
         multiple: true,
-        typeLabel: '{underline recipePath}',
+        typeLabel: '{underline recipePath[]}',
         description: 'path to recipe.json which should be loaded at startup'
     },
     {
@@ -53,52 +55,89 @@ const optionDefinitions = [
         alias: 'h',
         type: Boolean,
         description: 'Print this usage guide.'
+    },
+    {
+        name: 'watch',
+        alias: 'w',
+        type: String,
+        multiple: true,
+        typeLabel: '{underline opcuaEndpoint} {underline opcuaNodeid} {underline httpUri}',
+        description: 'Monitors an OPC UA node (specified via {underline opcuaNodeId}) on the OPC UA server (specified by {underline opcuaEndpoint}. If the node changes its value, a HTTP POST request is sent to {underline httpURI}.'
     }
 ];
-const options = commandLineArgs(optionDefinitions);
-if (options.help) {
-
-    const commandLineUsage = require('command-line-usage');
-
-    const sections = [
-        {
-            header: 'pfe-ree-node',
-            content: 'Starts recipe execution engine for controlling services of modules.'
-        },
-        {
-            header: 'Options',
-            optionList: optionDefinitions
-        }
-    ];
-    const usage = commandLineUsage(sections);
-    console.log(usage);
-} else {
-    const port: number | string | boolean = serverHandlers.normalizePort(process.env.PORT || 3000);
-
-    const appServer = new Server();
-    appServer.app.set('port', port);
-
-    const server: http.Server = http.createServer(appServer.app);
-
-    // initialize the WebSocket server instance
-    appServer.initSocketServer(server);
-
-    server.listen(port);
-    server.on('error', error => serverHandlers.onError(error, port));
-    server.on('listening', serverHandlers.onListening.bind(server));
-
-    /** Load some configuration at startup */
-    if (options.module) {
-        options.module.forEach((module) => {
-            const modulesOptions = JSON.parse(fs.readFileSync(module).toString());
-            manager.loadModule(modulesOptions);
-        });
+const sections = [
+    {
+        header: 'pfe-ree-node',
+        content: 'Starts recipe execution engine for controlling services of modules.'
+    },
+    {
+        header: 'Synopsis',
+        content: [
+            '$ node build/index.js [{bold --module} {underline modulePath}] [{bold --recipe} {underline recipePath}] [{bold --watch} {underline opcuaEndpoint} {underline opcuaNodeid} {underline httpUri}]'
+        ]
+    },
+    {
+        header: 'Options',
+        optionList: optionDefinitions
+    },
+    {
+        header: 'Examples',
+        content: [
+            {
+                desc: 'Watching a OPC UA server',
+                example: '$ node src/index.js --watch opc.tcp://127.0.0.1:53530/OPCUA/SimulationServer "ns=3;s=BooleanDataItem" 127.0.0.1:3000/api/startRecipe'
+            }]
     }
+];
 
-    if (options.recipe) {
-        options.recipe.forEach((recipe) => {
-            const recipeOptions = JSON.parse(fs.readFileSync(recipe).toString());
-            manager.loadRecipe(recipeOptions);
-        });
+let options;
+try {
+    options = commandLineArgs(optionDefinitions);
+} catch (err) {
+    console.log('Error: Could not parse command line arguments', err.toString());
+    console.log(commandLineUsage(sections));
+}
+if (options) {
+    if (options.help) {
+        console.log(commandLineUsage(sections));
+    } else {
+        const port: number | string | boolean = serverHandlers.normalizePort(process.env.PORT || 3000);
+
+        const appServer = new Server();
+        appServer.app.set('port', port);
+
+        const server: http.Server = http.createServer(appServer.app);
+
+        // initialize the WebSocket server instance
+        appServer.initSocketServer(server);
+
+        server.listen(port);
+        server.on('error', error => serverHandlers.onError(error, port));
+        server.on('listening', serverHandlers.onListening.bind(server));
+
+        /** Load some configuration at startup */
+        if (options.module) {
+            options.module.forEach((module) => {
+                const modulesOptions = JSON.parse(fs.readFileSync(module).toString());
+                manager.loadModule(modulesOptions);
+            });
+        }
+
+        if (options.recipe) {
+            options.recipe.forEach((recipe) => {
+                const recipeOptions = JSON.parse(fs.readFileSync(recipe).toString());
+                manager.loadRecipe(recipeOptions);
+            });
+        }
+
+        /** Start OPC UA flag watcher **/
+        if (options.watch) {
+            console.log('watch', options.watch);
+            const endpoint = options.watch[0];
+            const nodeid = options.watch[1];
+            const httpuri = options.watch[2];
+
+            watchFlag(endpoint, nodeid, httpuri);
+        }
     }
 }
