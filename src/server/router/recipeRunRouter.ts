b7ae7593e03c86760c324d5f473f607048c911f9
Markus Graube <markus.graube@tu-dresden.de>
2019-01-16 08:23:52 +0100

    update @plt/pfe-ree-interface
    Signed-off-by: Markus Graube <markus.graube@tu-dresden.de>

---
+++ src/server/router/recipeRunRouter.ts
@@ -0,0 +1,41 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Markus Graube <markus.graube@tu.dresden.de>,
+ * Chair for Process Control Systems, Technische Universität Dresden
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import { manager } from '../../model/Manager';
+import { Request, Response, Router } from 'express';
+
+export const recipeRunRouter: Router = Router();
+
+/**
+ * @api {get} /recipeRun/:recipeRunId    Get recipe run
+ * @apiName GetRecipeRun
+ * @apiGroup RecipeRun
+ * @apiParam recipeRunId
+ */
+recipeRunRouter.get('/:recipeRunId', async (req: Request, res: Response) => {
+    const result = manager.player.recipeRuns.find(recipeRun => recipeRun.id === req.params.recipeRunId).json();
+    res.contentType('application/json').attachment()
+        .send(JSON.stringify(result, null, 2));
+});
